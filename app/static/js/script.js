60b8cb4320416ea62e65584360d7cc4d8748950e
Jaclyn Burge <jacburge@gmail.com>
2017-09-23 13:15:00 -0700

    Updated art

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -4,28 +4,28 @@ $(document).ready(function(){
 		$('#nav-bar-mobile').slideToggle(400);
 	});
 
-	$('#photography-tab').click(function(){
-        $("#artwork").fadeOut(600);
-        $("#music").fadeOut(600);
-        $("#photography").fadeIn(600);
-        $('#artwork-tab').css("color","#333333");
-        $('#music-tab').css("color","#333333");
-        $('#photography-tab').css("color","#006666");
-        $('#photography-tab').css("text-decoration","underline");
-        $('#artwork-tab').css("text-decoration","none");
-        $('#music-tab').css("text-decoration","none");
-    });
-
 	$('#artwork-tab').click(function(){
         $("#photography").fadeOut(600);
         $("#music").fadeOut(600);
         $("#artwork").fadeIn(600);
         $('#photography-tab').css("color","#333333");
+        $('#photography-tab').css("text-decoration","none");
         $('#music-tab').css("color","#333333");
+        $('#music-tab').css("text-decoration","none");
         $('#artwork-tab').css("color","#006666");
         $('#artwork-tab').css("text-decoration","underline");
-        $('#photography-tab').css("text-decoration","none");
-        $('#music-tab').css("text-decoration","none");
+    });
+
+	$('#photography-tab').click(function(){
+        $("#artwork").fadeOut(600);
+        $("#music").fadeOut(600);
+        $("#photography").fadeIn(600);
+        $('#artwork-tab').css("color","#333333");
+        $('#artwork-tab').css("text-decoration","none");
+        $('#music-tab').css("color","#333333");
+         $('#music-tab').css("text-decoration","none");
+        $('#photography-tab').css("color","#006666");
+        $('#photography-tab').css("text-decoration","underline"); 
     });
 
 	$('#music-tab').click(function(){
@@ -33,11 +33,11 @@ $(document).ready(function(){
         $("#artwork").fadeOut(600);
         $("#music").fadeIn(600);
         $('#photography-tab').css("color","#333333");
+        $('#photography-tab').css("text-decoration","none");
         $('#artwork-tab').css("color","#333333");
+         $('#artwork-tab').css("text-decoration","none");
         $('#music-tab').css("color","#006666");
         $('#music-tab').css("text-decoration","underline");
-        $('#photography-tab').css("text-decoration","none");
-        $('#artwork-tab').css("text-decoration","none");
     });
 
 jQuery(window).on('load', function(){ 
@@ -46,7 +46,8 @@ jQuery(window).on('load', function(){
 		itemSelector : '.photo', 
 		gutter: 0});
 	});
-$('#photography-tab').click(function() {
+
+$('#artwork-tab').click(function() {
 	var container = document.querySelector('.photo-grid');
 	// initialize Masonry after all images have loaded
 	imagesLoaded( container, function() {
@@ -55,7 +56,7 @@ $('#photography-tab').click(function() {
 		gutter: 0});
 });
 });
-$('#artwork-tab').click(function() {
+$('#photography-tab').click(function() {
 	var container = document.querySelector('.photo-grid');
 	// initialize Masonry after all images have loaded
 	imagesLoaded( container, function() {
